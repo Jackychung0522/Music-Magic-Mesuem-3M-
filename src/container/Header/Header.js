c9f4b438510a5179af160edd634308f7dcaef39d
Jackychung0522 <stp77770z@gmail.com>
2023-05-29 19:03:36 +0800

    header

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Container } from "reactstrap";
 import "./Header.css";
-
+import m3 from  "../../image/3M.jpg"
 const Header = () => {
   return (
     <Container>
-      <div></div>
+       <header className="header">
+      <nav className="navigation">
+        <button className="btn" id="M3"> <img src={m3}/></button>
+        <button className="btn">Create By Yourself</button>
+        <button className="btn">Play</button>
+      </nav>
+    </header>
     </Container>
   );
 };
