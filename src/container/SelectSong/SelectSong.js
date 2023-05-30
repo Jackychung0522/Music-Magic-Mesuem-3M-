e827065c52935c8b0a683d91f38a00526af8c1b4
Jackychung0522 <stp77770z@gmail.com>
2023-05-30 11:31:54 +0800

    select song

--- src/container/SelectSong/SelectSong.js
+++ src/container/SelectSong/SelectSong.js
@@ -1,14 +1,34 @@
-import React from "react";
+import { React, useState } from "react";
 import { Container } from "reactstrap";
 import "./SelectSong.css";
 
-const  SelectSong= () => {
-  return (
-    <Container>
-      <p id="selectsong">
-      <span role="img" aria-label="fire">🔥</span> Select Song
-      </p>
-    </Container>
-  );
+const SelectSong = () => {
+    const songs = ["Song 1", "Song 2", "Song 3"];
+    const [selectedSongIndex, setSelectedSongIndex] = useState(0);
+
+    const handlePreviousSong = () => {
+        setSelectedSongIndex((prevIndex) => (prevIndex === 0 ? songs.length - 1 : prevIndex - 1));
+    };
+
+    const handleNextSong = () => {
+        setSelectedSongIndex((prevIndex) => (prevIndex === songs.length - 1 ? 0 : prevIndex + 1));
+    };
+    return (
+        <Container>
+            <p id="selectsong">
+                <span role="img" aria-label="fire">🔥</span> Select Song
+            </p>
+            <div className="songBlock">
+                <button id='up' className="triangleButton" onClick={handlePreviousSong}>&#9650;</button>
+                
+                <button id="songButton">
+                    <span className="songName">{songs[selectedSongIndex]}</span>
+                    <span className="difficulty">&#9733;10000</span>
+                </button>
+                <button id='down' className="triangleButton" onClick={handleNextSong}>&#9660;</button>
+            </div>
+
+        </Container>
+    );
 };
-export default  SelectSong;
+export default SelectSong;
