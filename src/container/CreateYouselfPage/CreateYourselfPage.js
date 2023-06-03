de576fc5e7ac5eba69ff8d3780055786c33d3251
austin <as0983228754@gmail.com>
2023-06-03 20:45:13 +0800

    change icon

--- src/container/CreateYouselfPage/CreateYourselfPage.js
+++ src/container/CreateYouselfPage/CreateYourselfPage.js
@@ -403,7 +403,7 @@ const CreateYourselfPage = () => {
               <button
                 // disabled
                 style={{
-                  backgroundColor: IsTempoRun ? "rgb(125, 178, 224)" : "",
+                  backgroundColor: IsTempoRun ? "rgb(181, 108, 72)" : "",
                 }}
                 id="pikachuButton"
                 onClick={handleSwitchOnTempoRun}
